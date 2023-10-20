3da72a5bddbb717095822f9dbd0e9db41b7adeee
Mugabo <mugabo@Mugabos-MacBook-Air.local>
2023-10-20 13:50:21 +0200

    Initial commit

--- src/dbank_backend/main.mo
+++ src/dbank_backend/main.mo
@@ -1,5 +1,47 @@
-actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Debug "mo:base/Debug";
+import Nat "mo:base/Nat";
+import Int "mo:base/Int";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+actor DBank {
+  stable var currentValue:Float = 300; // By default you dont need natural value (Nat) when it's a positive number 
+  // stable is an Orth persistance.. it saves the currentValue after topUp or Withdraw
+  currentValue := 100;
+  Debug.print(debug_show(currentValue));
+
+  let startTime = Time.now();
+  Debug.print(debug_show(startTime));
+
+  let id = 5674289023;
+
+  // Debug.print(debug_show(currentValue));
+  Debug.print(debug_show(id));
+
+  public func topUp(amount: Float) {
+    currentValue += amount;
+
+    Debug.print(debug_show(currentValue));
   };
-};
+
+  public func withdraw(amount: Float) {
+    let tempValue: Float = currentValue - amount;
+    if (tempValue >= 0) {
+      currentValue -= amount;
+
+      Debug.print(debug_show(currentValue))
+    } else {
+      Debug.print("The amount is larger than the balance.")
+    }
+  };
+
+  public query func checkBalance(): async Float {
+    return currentValue;
+  };
+
+  public func timeElapsed() {
+    let currentTime = Time.now()
+  }
+
+  // topUp();
+}
--- src/dbank_frontend/src/index.js
+++ src/dbank_frontend/src/index.js
@@ -1,19 +1,46 @@
+import { dbank_frontend } from "../../declarations/dbank_frontend";
 import { dbank_backend } from "../../declarations/dbank_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+window.addEventListener("load", async function () {
+  // console.log("Finished loading");
 
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await dbank_backend.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+  updateBalance();
 });
+
+document
+  .querySelector("form")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+    // console.log("Submitted");
+
+    const button = event.target.querySelector("#submit-btn");
+
+    const inputAmount = parseFloat(
+      document.getElementById("input-amount").value
+    );
+    const outputAmount = parseFloat(
+      document.getElementById("withdrawal-amount").value
+    );
+
+    button.setAttribute("disabled", true);
+
+    if (document.getElementById("input-amount").value.length != 0) {
+      await dbank_backend.topUp(inputAmount);
+    }
+
+    if (document.getElementById("withdrawal-amount").value.length != 0) {
+      await dbank_backend.withdraw(outputAmount);
+    }
+
+    updateBalance();
+
+    document.getElementById("input-amount").value = "";
+    document.getElementById("withdrawal-amount").value = "";
+    button.removeAttribute("disabled");
+  });
+
+async function updateBalance() {
+  const currentAmount = await dbank_backend.checkBalance();
+  document.getElementById("value").innerText =
+    Math.round(currentAmount * 100) / 100;
+}
